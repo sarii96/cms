cf8f75d7f45e36b6551226159065b3600aff7b82
sarii96 <sara-96325@hotmail.com>
2021-01-15 10:10:51 -0700

    hi

--- src/app/contacts/contact-list/contact-list.component.ts
+++ src/app/contacts/contact-list/contact-list.component.ts
@@ -7,7 +7,7 @@ import { Contact } from '../contact.model';
   styleUrls: ['./contact-list.component.css']
 })
 export class ContactListComponent implements OnInit {
-  Contacts: Contact[] = [
+  contacts: Contact[] = [
     new Contact('1', 'Brother Barzee', 'barzeer@byui.edu', '208-496-3768', 'https://web.byui.edu/Directory/Employee/barzeer.jpg', null),
     new Contact('2', 'Brother Jackson', 'jacksonk@byui.edu', '208-496-3771', 'https://web.byui.edu/Directory/Employee/jacksonk.jpg', null),
     new Contact('3', 'Brother Thayne', 'thayneti@byui.edu', '208-496-3777', 'http://web.byui.edu/Directory/Employee/thayneti.jpg', null),
