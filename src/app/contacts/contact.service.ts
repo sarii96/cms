795ffa3c9d6859cab5a5c5274275ca19300d46bc
sarii96 <sara-96325@hotmail.com>
2021-03-04 11:13:28 -0700

    week08 fix

--- src/app/contacts/contact.service.ts
+++ src/app/contacts/contact.service.ts
@@ -1,14 +1,13 @@
 import { Injectable } from '@angular/core';
-import { Subject} from 'rxjs';
-import {Contact} from './contact.model';
-import {MOCKCONTACTS} from './MOCKCONTACTS';
-
+import { Subject } from 'rxjs';
+import { Contact } from './contact.model';
+import { MOCKCONTACTS } from './MOCKCONTACTS';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ContactService {
-  private contacts: Contact[] =[];
+  private contacts: Contact[] = [];
 
   // contactSelectedEvent = new EventEmitter<Contact>();
   contactListChangedEvent = new Subject<Contact[]>();
@@ -19,8 +18,8 @@ export class ContactService {
     this.maxContactId = this.getMaxId();
   }
 
-  addContact(newContact: Contact){
-    if(!newContact){
+  addContact(newContact: Contact) {
+    if (!newContact) {
       return;
     }
     this.maxContactId++;
@@ -32,75 +31,71 @@ export class ContactService {
     const contactListClone = this.contacts.slice();
 
     this.contactListChangedEvent.next(contactListClone);
-
   }
 
   deleteContact(contact: Contact) {
     if (!contact) {
       return;
-   }
-   const pos = this.contacts.indexOf(contact);
+    }
+    const pos = this.contacts.indexOf(contact);
     if (pos < 0) {
-       return;
+      return;
     }
     this.contacts.splice(pos, 1);
 
     const contactsListClone = this.contacts.slice();
 
     this.contactListChangedEvent.next(contactsListClone);
-
-   }
-
+  }
 
   // getContacts(id: string): Contact[]{
   //   return this.contacts
   //   .sort((a, b) => a.name > b.name ? 1 : b.name > a.name ? -1: 0)
   //   .slice();
   // }
-getContact(id: string): Contact{
-  for(const contact of this.contacts){
-    if (contact.id === id){
-      return contact;
+  getContact(id: string): Contact {
+    for (const contact of this.contacts) {
+      if (contact.id === id) {
+        return contact;
+      }
     }
+    return null;
   }
-  return null;
-}
 
- getContacts(): Contact[]{
-   return this.contacts.slice();
- }
+  getContacts(): Contact[] {
+    return this.contacts.slice();
+  }
 
   getMaxId(): number {
-    let maxId= 0;
-    for(const contact of this.contacts){
+    let maxId = 0;
+    for (const contact of this.contacts) {
       let currentId = parseInt(contact.id);
-      if(currentId > maxId){
+      if (currentId > maxId) {
         maxId = currentId;
       }
     }
     return maxId;
   }
 
+  //   getContact(id:string): Contact {
+  // return this.contacts.find((contact)=> contact.id === id);
+  //   }
 
-//   getContact(id:string): Contact {
-// return this.contacts.find((contact)=> contact.id === id);
-//   }
+  updateContact(originalContact: Contact, newContact: Contact) {
+    if (!originalContact || !newContact) {
+      return;
+    }
 
-updateContact(originalContact: Contact, newContact: Contact){
-  if(!originalContact || !newContact) {
-    return;
+    const pos = this.contacts.indexOf(originalContact);
+
+    if (pos < 0) {
+      return;
+    }
+
+    newContact.id = originalContact.id;
+    this.contacts[pos] = newContact;
+
+    const contactsListClone = this.contacts.slice();
+    this.contactListChangedEvent.next(contactsListClone);
   }
-
-  const pos = this.contacts.indexOf(originalContact);
-
-  if (pos < 0){
-    return;
-  }
-
-  this.contacts[pos] = newContact;
-
-  const contactsListClone = this.contacts.slice();
-  this.contactListChangedEvent.next(contactsListClone);
-}
-
 }
